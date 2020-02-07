ef17b28b4ce8b00d690c6b4f0a67dfc924dd55fe
Vanshika2703 <reddyvs@rose-hulman.edu>
2020-02-06 23:12:49 -0500

    Neelie made the user login

---
+++ src/Components/UserLogin/UserLogin.component.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+console.log("im in here (top)")
+
+
+export default class UserLogin extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            Username: '',
+            Password: '',
+        };
+
+        this.setValue = this.setValue.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        // this.render()
+    }
+
+    setValue(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    onSubmit(event) {
+        console.log("on submit works")
+        event.preventDefault();
+        const newItem = {
+            Username: this.state.Username,
+            Password: this.state.Password,
+
+        }
+        console.log(UserLogin);
+        fetch(`api/user?username=${this.state.Username}';password=${this.state.Password}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then((res) => res.json())
+        .then((result) => {
+            if (result.success == true)
+                localStorage.setItem('user', JSON.stringify(result.user));
+            
+        },
+        (err) => {
+            console.log(err);
+        })
+        }
+
+
+    render() {
+        console.log("im in here (bottom)")
+        return(
+            <div>
+                <Container>
+                    <Form onSubmit={this.onSubmit}>
+                        <Form.Group controlId="Username">
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control name="Username" onChange={this.setValue} type="text" placeholder="Enter Username" />
+                        </Form.Group>
+                        <Form.Group controlId="Password">
+                            <Form.Label>Item Type:</Form.Label>
+                            <Form.Control name="Password" onChange={this.setValue} type="password" placeholder="must be atleast 8 characters long" />
+                        </Form.Group>
+                       
+                        <Button variant="info" type="submit" >Login!</Button>
+                    </Form>
+                </Container>
+            </div>
+        )
+    }
+}
