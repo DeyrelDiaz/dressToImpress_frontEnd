571fb813fe9db8bbeb673482d1e8d8a2e579f5b9
Vanshika2703 <reddyvs@rose-hulman.edu>
2020-02-19 20:46:44 -0500

    user info page

---
+++ src/Components/UserInfo/UserInfo.Compomemt.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import { Tabs, Tab, Container, Navbar, Nav, Table} from 'react-bootstrap';
+
+export default class UserInfo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <Navbar bg="info" variant="dark">
+                    <Navbar.Brand>Dress To Impress</Navbar.Brand>
+                    <Nav>
+                        <Nav.Link href="/home">Home</Nav.Link>
+                        <Nav.Link href="/newItem">Add Item</Nav.Link>
+                        <Nav.Link href="/cart">Cart</Nav.Link>
+                        <Nav.Link href="/user/profile">User Profile</Nav.Link>
+
+                    </Nav>
+                </Navbar>
+                <Container>
+                <Tabs id="controlled-tab-example" variant="tabs" >
+                    <Tab eventKey="info" title="User Profile" variant="dark">
+                    <Table striped bordered hover variant="dark">
+                            <thead>
+                                <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                                </tr>
+                                <tr>
+                                <td>3</td>
+                                <td colSpan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </Table>                    </Tab>
+                    <Tab eventKey="orders" title="Orders" variant="dark">
+                    </Tab>
+                </Tabs>
+                </Container>
+            </div>
+        )
+    }
+}
